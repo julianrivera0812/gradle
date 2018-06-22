1a279337820c6563f142fad0396fb6ddb98ab7d3
CEIBA\julian.rivera <julian.rivera@ceiba.com.co>
2018-06-22 09:07:50 -0500

    JUnit de la practica

---
+++ proyecto1/src/test/java/co/ceiba/testdatabuilder/PersonTestDataBuilder.java
@@ -0,0 +1,29 @@
+package co.ceiba.testdatabuilder;
+
+import co.ceiba.domain.Person;
+
+public class PersonTestDataBuilder {
+
+	private String name;
+
+	private String lastname;
+
+	public PersonTestDataBuilder() {
+		this.name = "Ceiba";
+		this.lastname = "Software House";
+	}
+
+	public PersonTestDataBuilder withName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public PersonTestDataBuilder withLastName(String lastname) {
+		this.lastname = lastname;
+		return this;
+	}
+
+	public Person build() {
+		return new Person(name, lastname);
+	}
+}
